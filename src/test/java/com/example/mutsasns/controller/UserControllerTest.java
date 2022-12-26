8a67425435a6349909d8629fddeaeeb1eb1047f6
Universenn <dnwndhks98@gmail.com>
2022-12-27 01:32:43 +0900

    login 성공 테스트 코드 구현

--- src/test/java/com/example/mutsasns/controller/UserControllerTest.java
+++ src/test/java/com/example/mutsasns/controller/UserControllerTest.java
@@ -2,7 +2,9 @@ package com.example.mutsasns.controller;
 
 import com.example.mutsasns.entity.dto.user.UserJoinRequest;
 import com.example.mutsasns.entity.dto.user.UserJoinResponse;
+import com.example.mutsasns.entity.dto.user.UserLoginResponse;
 import com.example.mutsasns.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -31,16 +34,19 @@ class UserControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    // given
+    String userName = "name";
+    String password = "q1w2e3r4";
+
+    String token = "token";
+    UserJoinRequest userJoinRequest = new UserJoinRequest(userName, password);
+    UserJoinResponse userJoinResponse = UserJoinResponse.of(userJoinRequest.toEntity());
+
 
     @Test
     @DisplayName("회원가입 성공")
     @WithMockUser
     void join() throws Exception {
-        // given
-        String userName = "name";
-        String password = "q1w2e3r4";
-        UserJoinRequest userJoinRequest = new UserJoinRequest(userName, password);
-        UserJoinResponse userJoinResponse = UserJoinResponse.of(userJoinRequest.toEntity());
 
         // when
         when(userService.add(userJoinRequest))
@@ -54,5 +60,23 @@ class UserControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk());
 
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    @WithMockUser
+    void login_success() throws Exception {
+
+        // when
+        when(userService.login(userJoinRequest))
+                .thenReturn(new UserLoginResponse(token));
+
+        // then
+        mockMvc.perform(post("/api/v1/users/login")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
