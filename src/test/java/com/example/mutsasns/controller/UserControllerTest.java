e4c7fa64c0f7603bbcbe3aa39b591f00296ff5c4
Universenn <dnwndhks98@gmail.com>
2022-12-27 00:21:23 +0900

    join test 코드 작성

---
+++ src/test/java/com/example/mutsasns/controller/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.example.mutsasns.controller;
+
+import com.example.mutsasns.entity.dto.user.UserJoinRequest;
+import com.example.mutsasns.entity.dto.user.UserJoinResponse;
+import com.example.mutsasns.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join() throws Exception {
+        // given
+        String userName = "name";
+        String password = "q1w2e3r4";
+        UserJoinRequest userJoinRequest = new UserJoinRequest(userName, password);
+        UserJoinResponse userJoinResponse = UserJoinResponse.of(userJoinRequest.toEntity());
+
+        // when
+        when(userService.add(userJoinRequest))
+                .thenReturn(userJoinResponse);
+
+        // then
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+}
