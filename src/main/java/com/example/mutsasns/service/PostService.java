31fd6ef2d5c4da8e09cc75efd1138109c5754101
Universenn <dnwndhks98@gmail.com>
2022-12-30 22:07:50 +0900

    PostService update 구현

--- src/main/java/com/example/mutsasns/service/PostService.java
+++ src/main/java/com/example/mutsasns/service/PostService.java
@@ -10,6 +10,7 @@ import com.example.mutsasns.repository.PostRepository;
 import com.example.mutsasns.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +22,19 @@ public class PostService {
     public PostResponse create(PostRequest dto, String userName) {
         User user = userRepository.findByUserName(userName).orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
         Post post = postRepository.save(dto.toEntity(user));
+        return PostResponse.of(post);
+    }
+
+    @Transactional
+    public PostResponse update(PostRequest dto, String userName, Long id) {
+        User user = userRepository.findByUserName(userName).orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
+
+        Post post = postRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND, ErrorCode.POST_NOT_FOUND.getMessage()));
+
+        if (!user.getId().equals(post.getId())) throw new AppException(ErrorCode.INVALID_TOKEN, ErrorCode.INVALID_TOKEN.getMessage());
+
+        post.update(dto.getTitle(), dto.getBody());
+
         return PostResponse.of(post);
     }
 }
