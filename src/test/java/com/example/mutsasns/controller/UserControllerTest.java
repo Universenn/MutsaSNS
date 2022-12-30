0e11eff8739ea356e80553c8b4272da9762e909e
Universenn <dnwndhks98@gmail.com>
2022-12-31 02:28:40 +0900

    Test 회원가입 성공

--- src/test/java/com/example/mutsasns/controller/UserControllerTest.java
+++ src/test/java/com/example/mutsasns/controller/UserControllerTest.java
@@ -1,29 +1,35 @@
 package com.example.mutsasns.controller;
 
-import com.example.mutsasns.entity.dto.user.UserRequest;
 import com.example.mutsasns.entity.dto.user.UserJoinResponse;
 import com.example.mutsasns.entity.dto.user.UserLoginResponse;
-import com.example.mutsasns.exception.AppException;
-import com.example.mutsasns.exception.ErrorCode;
+import com.example.mutsasns.entity.dto.user.UserRequest;
 import com.example.mutsasns.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(UserController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@WithMockUser
 class UserControllerTest {
 
     @Autowired
@@ -35,107 +41,28 @@ class UserControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    // given
-    String userName = "name";
-    String password = "q1w2e3r4";
+    UserRequest userRequest = new UserRequest("user1", "qwer1234");
 
-    String token = "token";
-    UserRequest userRequest = new UserRequest(userName, password);
-    UserJoinResponse userJoinResponse = UserJoinResponse.of(userRequest.toEntity());
-
+    UserLoginResponse userLoginResponse = new UserLoginResponse("token");
 
     @Test
     @DisplayName("회원가입 성공")
-    @WithMockUser
-    void join() throws Exception {
+    void join_success() throws Exception {
 
-        // when
-        when(userService.join(userRequest))
-                .thenReturn(userJoinResponse);
+        UserJoinResponse userJoinResponse = new UserJoinResponse(0L, userRequest.getUserName());
 
-        // then
-        mockMvc.perform(post("/api/v1/users/join")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(userRequest)))
+        when(userService.join(any())).thenReturn(userJoinResponse);
+
+        mockMvc.perform(
+                        post("/api/v1/users/join")
+                                .with(csrf())
+                                .content(objectMapper.writeValueAsBytes(userRequest))
+                                .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
-
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("SUCCESS"))
+                .andExpect(jsonPath("$.result.userName").value("user1"))
+                .andExpect(jsonPath("$.result.userId").value(0));
     }
 
-    @Test
-    @DisplayName("회원가입 실패 - DUPLICATED_USERNAME")
-    @WithMockUser
-    void join_fail() throws Exception {
-
-        // when
-        when(userService.join(any()))
-                .thenThrow(new AppException(ErrorCode.DUPLICATED_USERNAME, ErrorCode.DUPLICATED_USERNAME.getMessage()));
-
-        // then
-        mockMvc.perform(post("/api/v1/users/join")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(userRequest)))
-                .andDo(print())
-                .andExpect(status().is(ErrorCode.DUPLICATED_USERNAME.getHttpStatus().value()));
-
-    }
-
-    @Test
-    @DisplayName("로그인 성공")
-    @WithMockUser
-    void login_success() throws Exception {
-
-        // when
-        when(userService.login(userRequest))
-                .thenReturn(new UserLoginResponse(token));
-
-        // then
-        mockMvc.perform(post("/api/v1/users/login")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(userRequest)))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("로그인 실패_USERNAME_NOTFOUND")
-    @WithMockUser
-    void login_fail_1() throws Exception {
-
-//        userJoinRequest = new UserJoinRequest(userJoinRequest.getUserName(), "틀림");
-
-        // when
-        when(userService.login(any()))
-                .thenThrow(new AppException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
-
-        // then
-        mockMvc.perform(post("/api/v1/users/login")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(userRequest)))
-                .andDo(print())
-                .andExpect(status().is(ErrorCode.USERNAME_NOT_FOUND.getHttpStatus().value()));
-    }
-
-
-    @Test
-    @DisplayName("로그인 실패_INVALID_PASSWORD")
-    @WithMockUser
-    void login_fail_2() throws Exception {
-
-        // when
-        when(userService.login(any()))
-                .thenThrow(new AppException(ErrorCode.INVALID_PASSWORD, ErrorCode.INVALID_PASSWORD.getMessage()));
-
-        // then
-        mockMvc.perform(post("/api/v1/users/login")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(userRequest)))
-                .andDo(print())
-                .andExpect(status().is(ErrorCode.INVALID_PASSWORD.getHttpStatus().value()));
-    }
 }
