a47385ae605fb343667eed052a0e266875e84f8e
Universenn <dnwndhks98@gmail.com>
2022-12-30 22:50:31 +0900

    PostController delete 생성

--- src/main/java/com/example/mutsasns/service/PostService.java
+++ src/main/java/com/example/mutsasns/service/PostService.java
@@ -9,6 +9,8 @@ import com.example.mutsasns.exception.ErrorCode;
 import com.example.mutsasns.repository.PostRepository;
 import com.example.mutsasns.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,4 +39,17 @@ public class PostService {
 
         return PostResponse.of(post);
     }
+
+    public PostResponse deletePost(String userName, Long id) {
+        User user = userRepository.findByUserName(userName).orElseThrow(() -> new AppException(ErrorCode.USERNAME_NOT_FOUND, ErrorCode.USERNAME_NOT_FOUND.getMessage()));
+
+        Post post = postRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND, ErrorCode.POST_NOT_FOUND.getMessage()));
+
+        if (!user.getId().equals(post.getId())) throw new AppException(ErrorCode.INVALID_TOKEN, ErrorCode.INVALID_TOKEN.getMessage());
+
+        postRepository.deleteById(post.getId());
+
+        return PostResponse.of(post);
+    }
+
 }
