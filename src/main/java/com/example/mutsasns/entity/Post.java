fb233c98de64348a7961d1052364a80647cf6641
Universenn <dnwndhks98@gmail.com>
2023-01-16 23:52:30 +0900

    soft delete

--- src/main/java/com/example/mutsasns/entity/Post.java
+++ src/main/java/com/example/mutsasns/entity/Post.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.REMOVE;
 
 @Entity
 @Getter
@@ -17,6 +23,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @EntityListeners(AuditingEntityListener.class)
+@Where(clause = "deleted = false")
+@SQLDelete(sql = "UPDATE Post SET deleted = true WHERE id = ?")
 public class Post {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,7 +36,7 @@ public class Post {
     @Column(nullable = false)
     private String body;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "user_id")
     private User user;
 
@@ -38,8 +46,18 @@ public class Post {
     @LastModifiedDate @Column(nullable = false)
     private LocalDateTime lastModifiedBy;
 
+    @OneToMany(mappedBy = "post",cascade = REMOVE, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "post",cascade = REMOVE, orphanRemoval = true)
+    private List<Likes> likes = new ArrayList<>();
+
+
+
     public void update(String title, String body) {
         this.title = title;
         this.body = body;
     }
+
+    private boolean deleted = Boolean.FALSE; // 삭제 여부 기본값 false
 }
